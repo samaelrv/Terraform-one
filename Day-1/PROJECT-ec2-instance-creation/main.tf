d1b75007a75e7aa60c0474cce35dd239ac5849f0
Ramanath Naik <88053906+samaelrv@users.noreply.github.com>
2024-10-14 22:28:10 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,12 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "us-east-2"  # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0e86e20dae9224db8"  # Specify an appropriate AMI ID
-    instance_type = "t3.micro"
+    ami           = "ami-09da212cf18033880"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "Hello"
+    }
 }
